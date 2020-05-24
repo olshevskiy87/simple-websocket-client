3181e2abd41b26b59de6cd0703604d4749b9da36
Dmitriy Olshevskiy <olshevskiy87@bk.ru>
2020-05-24 23:57:06 +0300

    index.js: add missing space symbol in datetime

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ const getNowDateStr = function () {
     let res = `${now.getFullYear()}\
 -${String(now.getMonth() + 1).padStart(2, '0')}\
 -${String(now.getDate()).padStart(2, '0')}\
-${String(now.getHours()).padStart(2, '0')}\
+ ${String(now.getHours()).padStart(2, '0')}\
 :${String(now.getMinutes()).padStart(2, '0')}\
 :${String(now.getSeconds()).padStart(2, '0')}`;
     if (showMsgTsMilliseconds.is(':checked')) {
